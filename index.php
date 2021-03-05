b8e10fd33a539968b02f60249276a12c6a76bcac
LucasHumbert <lucas.humbert.28@gmail.com>
2021-03-05 19:10:44 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Epoka</title>
+</head>
+<body>
+    
+    <!---- HEADER ---->
+    <header>
+        <div id="links">
+            <div>
+                <p>Connexion</p>
+            </div>
+
+            <div>
+                <p>Validation des missions</p>
+            </div>
+
+            <div>
+                <p>Paiement des frais</p>
+            </div>
+
+            <div>
+                <p>Paramétrage</p>
+            </div>
+        </div>
+
+        <div id="copyright">
+            Copyright 2021 Humbert Lucas
+        </div>
+    </header>
+
+</body>
+</html>
---
+++ services/serv_authentification-mobile.php
@@ -0,0 +1,22 @@
+<?php
+    
+    if (!isset($_GET["user"])) die ("#User absent");
+    $user = $_GET["user"];
+    if (!isset($_GET["mdp"])) die ("#Mdp absent");
+    $mdp = $_GET["mdp"];
+
+    $pdo = new PDO("mysql:host=127.0.0.1; dbname=epoka;charset=UTF8", "root", "root");
+    $stmt = $pdo->prepare ("SELECT * FROM salarie WHERE sal_id=:user AND sal_mdp=PASSWORD(:mdp)");
+    $stmt->bindParam ("user", $user,PDO::PARAM_STR);
+    $stmt->bindParam ("mdp", $mdp,PDO::PARAM_STR);
+    $stmt->execute ();
+
+    if ($ligne = $stmt->fetch()){
+        
+        echo($ligne["sal_nom"]." ".$ligne["sal_prenom"]);
+        
+    } else {
+        die("#Erreur connexion");
+    }
+
+?>
